099f0bc53832d585eeed797a5bdc406508dc61fb
miji <mijilee0413@sookmyung.ac.kr>
2023-11-12 22:08:11 +0900

    10-6(1)
    한 글자씩 읽어와서 출력

--- 10main.c
+++ 10main.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-//실습5
-
+//실습6
+//한 글자씩 읽어와서 출력
 int main(void) {
-    // 파일 포인터 정의
-    FILE *filePointer;
+    FILE *fp = NULL;
+    char c;
 
     // 파일 열기
-    filePointer = fopen("sample.txt", "w");
-
-    // 파일 열기에 실패한 경우
-    if (filePointer == NULL) {
-        printf("Cannot open the file.\n");
-        return 1;  // 프로그램 종료, 오류 코드 반환
+    fp = fopen("sample.txt", "r");
+    if (fp == NULL) {
+        printf("파일을 열 수 없음\n");
+        return 1;  // 오류 코드 반환
     }
 
-    // 3개의 단어 입력 받아 파일에 쓰기
-    for (int i = 0; i < 3; i++) {
-        char word[50];
-        printf("Input a word: ");
-        scanf("%s", word);
-
-        // 파일에 내용 쓰기
-        fprintf(filePointer, "%s\n", word);
+    // 한 글자씩 읽어와서 출력
+    while ((c = fgetc(fp)) != EOF) {
+        putchar(c);
     }
 
     // 파일 닫기
-    fclose(filePointer);
-
-    printf("Successfully wrote content to the file.\n");
+    fclose(fp);
 
     return 0;
 }
