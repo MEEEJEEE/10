071355146f0155286bca787d68106a7f3ee54c5e
miji <mijilee0413@sookmyung.ac.kr>
2023-11-12 21:44:04 +0900

    10-2

--- 10main.c
+++ 10main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-//실습1
-
+//실습2
+//문자열을 초기화하고, 그후에 문자열을 반벅하여 하나씩 출력 
 int main(void) {
-	char code1 = 'A';
-    int code2 = 65;
-	
-	printf("code1 = %c, (%d)\n", code1, code1);
-    printf("code2 = %c, (%d)\n", code2, code2);
+    int i = 0;
+    char str[4];
+    str[0] = 'a';
+    str[1] = 'b';
+    str[2] = 'c';
+    str[3] = '\0';
+    
+    printf("%s\n", str);
 
+    // 현재 문자 (str[i])이 맨 끝이 아닐 때까지 반복
+    while (str[i] != '\0') {
+        // 현재 문자 하나 출력
+        printf("%c", str[i]);
+        
+        // i를 1 증가
+        i++;
+    }
+
+    // 반복이 끝나면 줄 바꿈
+    printf("\n");
+
+    return 0;
 }
